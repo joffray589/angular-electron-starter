530c63719bc2ec30e9822011134503d160447e6e
jbraga <joffray.braga@gmail.com>
2018-01-29 17:05:27 +0100

    sb-admin-2 integration

--- src/ng-front/app/app.module.ts
+++ src/ng-front/app/app.module.ts
@@ -1,18 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
