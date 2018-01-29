b7e6a3e69c986ad0aab80d5d737a8657c6f7b2e1
jbraga <joffray.braga@gmail.com>
2018-01-29 17:58:12 +0100

    add some sample links and routes

--- src/ng-front/app/app.module.ts
+++ src/ng-front/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import {AppRoutingModule} from './app.routing';
+
+import { SamplePage1Component } from './sample-page-1/sample-page-1.component';
+import { SamplePage2Component } from './sample-page-2/sample-page-2.component';
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    SamplePage1Component,
+    SamplePage2Component
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/ng-front/app/app.routing.ts
+++ src/ng-front/app/app.routing.ts
@@ -1,7 +1,18 @@
 import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {SamplePage1Component} from './sample-page-1/sample-page-1.component';
+import {SamplePage2Component} from './sample-page-2/sample-page-2.component';
+
+const routes: Routes = [
+  {path: 'sample-page-1', component: SamplePage1Component},
+  {path: 'sample-page-2', component: SamplePage2Component},
+
+  { path: '', redirectTo: '/sample-page-1', pathMatch: 'full' },
+];
 
 @NgModule({
+  imports: [RouterModule.forRoot(routes)],
   exports: [ RouterModule ]
 })
 export class AppRoutingModule {}
+
